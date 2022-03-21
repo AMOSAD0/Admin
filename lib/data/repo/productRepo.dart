d144f4541227b0347600dae1c740f2ba44398d02
AMOSAD0 <mossadahmed975@gmail.com>
2022-03-21 14:43:19 +0200

    m3

--- lib/data/repo/productRepo.dart
+++ lib/data/repo/productRepo.dart
@@ -1,31 +1,29 @@
-import 'package:admin/data/api/productApi.dart';
+/*import 'package:admin/data/api/productApi.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 import '../model/product.dart';
 
 class productRepo{
- QuerySnapshot<Map<String,dynamic>>? Val;
+ //QuerySnapshot<Map<String,dynamic>>? Val;
 List<product> products=[];
 productRepo(this.products);
   getProd(){
   productApi api= productApi() ;
-  api.getProudcts().then((value){
-    value.docs.forEach((e){
-    product p = product.fromJson(e.data());
+  api.getProudcts().docs.forEach((e){
+     product p = product.fromJson(e.data());
     p.id=e.id;
-   // p.Name=e.Name;
-    //p.Price=e.Price;
-    products.add(p);
-  });
-  //return products;
   });
  /* Val!.docs.forEach((e){
     product p = product.fromJson(e.data());
     p.id=e.id;
-   // p.Name=e.Name;
-    //p.Price=e.Price;
+    p.Name=e.Name;
+    p.Price=e.Price;
     products.add(p);
   });*/
     return products;
   }
 }
+
+
+
+*/
--- lib/presentation/module/AddProductScreen.dart
+++ lib/presentation/module/AddProductScreen.dart
@@ -1,5 +1,7 @@
 import 'package:admin/domin/product%20bloc/getproduct_Cubit.dart';
+import 'package:admin/domin/product%20bloc/getproduct_Stat.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:sizer/sizer.dart';
 class AddProductScreen extends StatelessWidget {
    AddProductScreen({Key? key}) : super(key: key);
@@ -7,22 +9,43 @@ class AddProductScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(  body: Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-       Padding(
-         padding: const EdgeInsets.all(50.0),
-         child: InkWell(
-           onTap: (){
-          p= ProductCubit().getAll();
-          print(p);
-           },
+    return BlocBuilder<ProductCubit,productStats>(
+      builder: (context, state) {
+       return    Scaffold(  body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+         Padding(
+           padding: const EdgeInsets.all(50.0),
+           child: InkWell(
+             onTap: (){
+            ProductCubit().getAll();
+            print(p);
+             },
+             child: Container(
+              height: 20.h,
+              width: 85.w,
+              color: Colors.grey[400],
+              child:const Center(
+                child:  Text('Product',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 30,
+                  color: Colors.black,
+                ),
+                ),
+              ),
+                 ),
+           ),
+         ),
+         //======================================//
+          Padding(
+           padding: const EdgeInsets.all(50.0),
            child: Container(
             height: 20.h,
             width: 85.w,
             color: Colors.grey[400],
             child:const Center(
-              child:  Text('Product',
+              child:  Text('Order',
               style: TextStyle(
                 fontWeight: FontWeight.bold,
                 fontSize: 30,
@@ -30,29 +53,13 @@ class AddProductScreen extends StatelessWidget {
               ),
               ),
             ),
-               ),
+        ),
          ),
-       ),
-       //======================================//
-        Padding(
-         padding: const EdgeInsets.all(50.0),
-         child: Container(
-          height: 20.h,
-          width: 85.w,
-          color: Colors.grey[400],
-          child:const Center(
-            child:  Text('Order',
-            style: TextStyle(
-              fontWeight: FontWeight.bold,
-              fontSize: 30,
-              color: Colors.black,
-            ),
-            ),
-          ),
+        ],
       ),
-       ),
-      ],
-    ),
-    );
+      );
+      });
+    
+    
   }
 }
