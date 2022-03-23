f6c64d2641cde9804071ab9b06c9072520e12467
AMOSAD0 <mossadahmed975@gmail.com>
2022-03-24 00:24:38 +0200

    ana el3mlto msh 7seen

--- lib/presentation/module/Products.dart
+++ lib/presentation/module/Products.dart
@@ -18,16 +18,49 @@ class Products extends StatelessWidget {
        builder: (context, state) {
        
       return Scaffold(
-        body: GridView.builder(gridDelegate:SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
-        itemCount: ProductCubit.get(context).P_List.length,
-         itemBuilder:(context,index){
-           return Container(
-             height: 20.h,
-             width: 20.w,
-             color: Colors.blue,
-             child: Text(ProductCubit.get(context).P_List[index].Name!),
-           );
-         } ),
+        body: Column(
+          children: [
+            InkWell(
+              onTap: () {
+                ProductCubit().addProduct();
+               // ProductCubit.get(context).getAll();
+              },
+              child: Container(
+                height: 20.h,
+                 width: 20.w,
+                 color: Colors.black,
+              ),
+            ),
+            Container(
+              height: 70.h,
+              width: 100.w,
+              child: GridView.builder(gridDelegate:SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
+              itemCount: ProductCubit.get(context).P_List.length,
+               itemBuilder:(context,index){
+                 return Container(
+                   height: 20.h,
+                   width: 20.w,
+                   color: Colors.blue,
+                   child: Column(
+                     children: [
+                       Text(ProductCubit.get(context).P_List[index].Name!),
+                       InkWell(
+                         onTap: () {
+                           ProductCubit().deleteProduct(ProductCubit.get(context).P_List[index].id!);
+                         //  ProductCubit.get(context).P_List.removeAt(index);
+                           
+                         },
+                         child: CircleAvatar(
+                           child: Icon(Icons.delete_forever),
+                         ),
+                       )
+                     ],
+                   ),
+                 );
+               } ),
+            ),
+          ],
+        ),
         
       );
        }
